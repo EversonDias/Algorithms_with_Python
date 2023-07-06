01a520c4ee683bf04250d5c983bdeedad2687982
everson <eversond14s@gmail.com>
2023-07-06 18:37:16 -0300

    Feat(challenges/challenge_palindromes_recursive): i created a function verify word is palindromes

--- challenges/challenge_palindromes_recursive.py
+++ challenges/challenge_palindromes_recursive.py
@@ -1,3 +1,9 @@
 def is_palindrome_recursive(word, low_index, high_index):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if len(word) > 99:
+        raise RecursionError
+    if not word:
+        return False
+    word_reverse = word[::-1]
+    if word_reverse != word:
+        return False
+    return True
