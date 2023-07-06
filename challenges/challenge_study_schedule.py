1371ac5c55882e6c45ef0fb8d5cdbbb2e2273de8
everson <eversond14s@gmail.com>
2023-07-06 15:38:57 -0300

    completed requirement 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -8,4 +8,3 @@ def study_schedule(permanence_period, target_time):
         if target_time >= period[0] and target_time <= period[1]:
             count += 1
     return count
-    raise NotImplementedError
