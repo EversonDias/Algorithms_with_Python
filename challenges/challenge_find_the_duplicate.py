0ddc4d88518e4ed821dff9a1715e9fef727f708f
everson <eversond14s@gmail.com>
2023-07-06 20:14:14 -0300

    Feat: I created a function that returs the duplicate number

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,16 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or len(nums) < 2:
+        return False
+
+    count = {}
+
+    for number in nums:
+        if type(number) == str or number < 0:
+            return False
+
+        if number in count:
+            return number
+        else:
+            count[number] = 1
+
+    return False
