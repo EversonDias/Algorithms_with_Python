18a6cd26135f77555aec6b059fda4751e7f8b343
everson <eversond14s@gmail.com>
2023-07-06 17:26:56 -0300

    completed requirement 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,9 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+import pytest
+from challenges.challenge_encrypt_message import encrypt_message
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message('helloworld', 4) == "dlrowo_lleh"
+    assert encrypt_message('helloworld', 11) == "dlrowolleh"
+    with pytest.raises(TypeError):
+        encrypt_message('hello world', 'oi')
